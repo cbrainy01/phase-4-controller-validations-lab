8b62427cb96eacd588f7d4b18e76d13c4650c114
Carl <carlbrainegebe@gmail.com>
2021-08-22 00:14:13 -0500

    commit tree 05cc7e69054548c1da4a7466c4d88bfae395722a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,7 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :mutombo
+
+
   def show
     author = Author.find(params[:id])
 
@@ -7,7 +9,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
@@ -16,6 +18,11 @@ class AuthorsController < ApplicationController
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def mutombo(invalid)
+    # invalid.record.errors.full_messages
+    render json: { errors: invalid.record.errors }, status: 422
   end
   
 end
